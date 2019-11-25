7e0d8366a64029b1d553392af12e0be85f7875e4
James Johnston <jamestheprogrammer@outlook.com>
2019-11-24 22:31:09 -0600

    Added comments to Parcel, and added GroundParcel contents.

--- Main/GroundParcel.h
+++ Main/GroundParcel.h
@@ -1,8 +1,32 @@
-#pragma once
-class GroundParcel
+/*
+* GroundParcel.h
+* Written by: James Johnston
+* Written for: COMPSCI 222 Project 3
+* Created on: 11/24/2019
+*/
+
+/*
+* Ground Parcel class definition
+*/
+
+#ifndef GROUND_PARCEL_H
+#define GROUND_PARCEL_H
+#include "Parcel.h"
+
+class GroundParcel : public Parcel
 {
 public:
+	// Constructors
 	GroundParcel();
+	GroundParcel(int number, std::string name, Contact sender, Contact receiver,
+		double fee, double weight, double oz);
+
+	// Destructors
 	~GroundParcel();
+
+	//	Overrides
+	double calculateCost();
+	std::string toString();
 };
 
+#endif
