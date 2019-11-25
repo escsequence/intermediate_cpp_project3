c5be1b28ff8b68ac95aa8a0a4700f74ce125e052
Ochowskiam19 <Ochowskiam19@MG125-05-LP>
2019-11-25 13:59:17 -0600

    Input stuff done

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -29,11 +29,12 @@ int main()
 	// Create vectors to store the parcels the user makes
 	vector<GroundParcel> ground;
 	vector<OvernightParcel> overnight;
+	vector<Contact> contacts;
 
 	// Begin the program's main loop to construct parcels
 	do {
 		// Show the user a list of options
-		cout << "What would you like to do now? Enter the corresponding number:\n\t1). Create a ground parcel\n\t2). Create an overnight parcel\n\t3). Display parcel information and quit." << endl;
+		cout << "What would you like to do now? Enter the corresponding number:\n\t1). Create a ground parcel\n\t2). Create an overnight parcel\n\t3). Display parcel information and quit" << endl;
 
 		// Ask user to make their choice
 		getline(cin, input);
@@ -42,22 +43,14 @@ int main()
 		switch (choice) {
 			case 1:
 			{
-				// Collect information
-				cout << "Please input the requested information: " << endl;
-				
-				// Assemble a new ground parcel from the inputted information
-				GroundParcel g();
-				//ground.push_back(g);
+				// Call a special method to collect all the data the program needs to make a ground parcel
+				createGroundParcel();
 				break;
 			}
 			case 2:
 			{
-				// Collect information
-				cout << "Please input the requested information: " << endl;
-				
-				// Assemble a new overnight parcel from the inputted information
-				OvernightParcel o();
-				//overnight.push_back(o);
+				// Call a special method to collect all the data the program needs to make an overnight parcel
+				createOvernightParcel();
 				break;
 			}
 			case 3: 
@@ -88,4 +81,191 @@ int main()
 
 	// End program
 	return -1;
+}
+
+void createGroundParcel() 
+{
+	// Create variables to gather input
+	int val = 0;
+	string input = "";
+	
+	// Collect information
+	cout << "Please input the requested information: " << endl;
+	
+	// Parcel Number
+	cout << "Parcel Number: #";
+	getline(cin, input);
+	int parcelNumber = stoi(input);
+
+	// Name of Parcel
+	cout << endl << "Name of Parcel: ";
+	string name;
+	getline(cin, name);
+
+	// Contact information for sender
+	cout << endl << "Who is the sender? ";
+	Contact senderAddress = handleContact();
+
+	// Contact information for recipient
+	cout << endl << "Who is the recipient? ";
+	Contact receiverAddress = handleContact();
+
+	// Fee
+	cout << endl << "Shipping fee: $";
+	getline(cin, input);
+	double fee = stod(input);
+
+	// Weight
+	cout << endl << "Weight (in oz): ";
+	getline(cin, input);
+	double weight = stod(input);
+
+	// Cost per ounce
+	cout << endl << "Cost per oz: $";
+	getline(cin, input);
+	double costPerOz = stod(input);
+	
+	// Assemble a new ground parcel from the inputted information
+	GroundParcel g(parcelNumber, name, senderAddress, receiverAddress, fee, weight, costPerOz);
+
+	// Push the ground parcel to the vector
+	//ground.push_back(g);
+	cout << endl << endl << "Thank you! Your ground parcel has been successfully created." << endl;
+}
+
+void createOvernightParcel()
+{
+	// Create variables to gather input
+	int val = 0;
+	string input = "";
+
+	// Collect information
+	cout << "Please input the requested information: " << endl;
+
+	// Parcel Number
+	cout << "Parcel Number: #";
+	getline(cin, input);
+	int parcelNumber = stoi(input);
+
+	// Name of Parcel
+	cout << endl << "Name of Parcel: ";
+	string name;
+	getline(cin, name);
+
+	// Contact information for sender
+	cout << endl << "Who is the sender? ";
+	Contact senderAddress = handleContact();
+
+	// Contact information for recipient
+	cout << endl << "Who is the recipient? ";
+	Contact receiverAddress = handleContact();
+
+	// Fee
+	cout << endl << "Shipping fee: $";
+	getline(cin, input);
+	double fee = stod(input);
+
+	// Weight
+	cout << endl << "Weight (in oz): ";
+	getline(cin, input);
+	double weight = stod(input);
+
+	// Cost per ounce
+	cout << endl << "Cost per oz: $";
+	getline(cin, input);
+	double costPerOz = stod(input);
+
+	// Tracking number
+	cout << endl << "Tracking Number: #";
+	getline(cin, input);
+	int trackingNumber = stoi(input);
+
+	// Standard weight
+	cout << endl << "Standard Weight (in oz): ";
+	getline(cin, input);
+	double standardWeight = stod(input);
+
+	// Assemble a new ground parcel from the inputted information
+	OvernightParcel o(parcelNumber, name, senderAddress, receiverAddress, fee, weight, costPerOz, trackingNumber, standardWeight);
+
+	// Push the ground parcel to the vector
+	//overnight.push_back(o);
+	cout << endl << endl << "Thank you! Your ground parcel has been successfully created." << endl;
+}
+
+Contact handleContact() 
+{
+	// Declare an initialize the basic variables that will be required to run the loop and intake user choices
+	bool go = true;
+	int choice = 0;
+	string input = "";
+	Contact userChoice;
+	
+	// Loop to ensure operation is completed successfully
+	do {
+		// Show the user a list of options
+		cout << "What would you like to do now? Enter the corresponding number:\n\t1). View existing contacts\n\t2). Create a new contact" << endl;
+
+		// Ask user to make their choice
+		getline(cin, input);
+		choice = stoi(input);
+
+		// Evaluate choice
+		switch (choice) {
+		case 1:
+		{
+			// Choose an existing conact
+			break;
+		}
+		case 2:
+		{
+			// Collect information
+			cout << "Please input the requested information: " << endl;
+
+			// Name of Contact
+			cout << endl << "Name of Contact: ";
+			string name;
+			getline(cin, name);
+
+			// Street Address of Contact
+			cout << endl << "Street Address: ";
+			string addressStreet;
+			getline(cin, addressStreet);
+
+			// City of Contact
+			cout << endl << "City: ";
+			string addressCity;
+			getline(cin, addressCity);
+
+			// State of Contact
+			cout << endl << "State: ";
+			string addressState;
+			getline(cin, addressState);
+
+			// Zip code of Contact
+			cout << endl << "Zip Code: ";
+			string addressZip;
+			getline(cin, addressZip);
+
+			// Create a contact with the given information
+			Contact userChoice(name, addressStreet, addressCity, addressState, addressZip);
+
+			// Add the contact to list of created contacts
+			//contacts.push_back(userChoice);
+
+			// Break loop
+			go = false;
+			break;
+		}
+		default:
+		{
+			// Inform the user that their input is invalid
+			cout << "Sorry, that's not a valid choice. Please try again." << endl;
+			break;
+		}
+		}
+	} while (go == true);
+
+	// Return the newly created contact or the contact selected by the user.
+	return userChoice;
 }
