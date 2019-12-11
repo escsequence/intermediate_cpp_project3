b6bacd6ce7bf30da6ee1e5d84f15edc04fb5d8b4
James Johnston <jamestheprogrammer@outlook.com>
2019-12-11 14:40:29 -0600

    ye

--- Main/ParcelIO.cpp
+++ Main/ParcelIO.cpp
@@ -28,9 +28,9 @@ void ParcelIO::setFileLocation(std::string groundFile, std::string overnightFile
   groundParcelFileName = groundFile;
   overnightParcelFileName = overnightFile;
   contactFileName = contactFile;
-  groundParcelFile = new VectorIO<GroundParcel>(groundFile);
-  overnightParcelFile = new VectorIO<OvernightParcel>(overnightFile);
-  contactFile = new VectorIO<OvernightParcel>(contactFile);
+  // groundParcelFile = new VectorIO<GroundParcel>(groundFile);
+  // overnightParcelFile = new VectorIO<OvernightParcel>(overnightFile);
+  // contactFile = new VectorIO<OvernightParcel>(contactFile);
 }
 
 // This function writes both contacts and parcels
